9fe58626d072f8ecbd003861f71fb7d9fd0fb318
narrowtamps <narrowtamps@gmail.com>
2026-02-25 01:54:18 -0600

    Update script.js

--- pages/script.js
+++ pages/script.js
@@ -38,12 +38,13 @@ style.textContent = `
 document.head.appendChild(style);
 document.getElementById('footer-placeholder').innerHTML = `
 <footer class="site-footer" style="margin-top: 50px; text-align: center;">
-    <p>&copy; 2026 The Narrow Tamps. All rights reserved.
+    <p></p>
+    &copy; 2026 The Narrow Tamps. All rights reserved.
     <div class="footer-links">
         <a href="../pages/campaigns.html">Campaigns</a>
             <a href="../pages/socials.html">Socials</a>
             <a href="../index.html">Main</a>
             <a href="../fish.html">Fish</a>
-            </p>
+            
     </div>
 </footer>`;
