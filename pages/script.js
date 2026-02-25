9f18725c1559a097bc5fc02685253a975fa6abcc
narrowtamps <narrowtamps@gmail.com>
2026-02-24 23:20:03 -0600

    Update script.js

--- pages/script.js
+++ pages/script.js
@@ -2,7 +2,9 @@ document.getElementById('footer-placeholder').innerHTML = `
 <footer class="site-footer" style="margin-top: 50px; text-align: center;">
     <p>&copy; 2024 My Website. All rights reserved.</p>
     <div class="footer-links">
-        <a href="index.html" style="color: GreenYellow; margin: 0 10px;">Home</a>
-        <a href="about.html" style="color: GreenYellow; margin: 0 10px;">About</a>
+        <a href="../pages/campaigns.html">Campaigns</a>
+            <a href="../pages/socials.html">Social s</a>
+            <a href="../home.html">Main</a>
+            <a href="../fish.html">Fish</a>
     </div>
 </footer>`;
