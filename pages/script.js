afd90990ccc0c3ec9fe25aa051a003a17942085a
narrowtamps <narrowtamps@gmail.com>
2026-02-24 23:31:33 -0600

    Update script.js

--- pages/script.js
+++ pages/script.js
@@ -1,10 +1,10 @@
 document.getElementById('footer-placeholder').innerHTML = `
 <footer class="site-footer" style="margin-top: 50px; text-align: center;">
-    <p>&copy; 2024 My Website. All rights reserved.</p>
+    <p>&copy; 2026 The Narrow Tamps. All rights reserved.</p>
     <div class="footer-links">
         <a href="../pages/campaigns.html">Campaigns</a>
-            <a href="../pages/socials.html">Social s</a>
-            <a href="../home.html">Main</a>
+            <a href="../pages/socials.html">Socials</a>
+            <a href="../index.html">Main</a>
             <a href="../fish.html">Fish</a>
     </div>
 </footer>`;
