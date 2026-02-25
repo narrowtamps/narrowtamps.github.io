39f3924a6f9fdc3c0df5f4eea2b16127c9bc2aec
narrowtamps <narrowtamps@gmail.com>
2026-02-25 01:58:37 -0600

    Update script.js

--- pages/script.js
+++ pages/script.js
@@ -18,7 +18,7 @@ style.textContent = `
     .site-footer p {
         margin: 0; /* Removes default gap between 1st and 2nd line */
         font-size: 0.9rem;
-        padding-bottom: 15px; 
+        padding-bottom: 25px; 
     }
 
     .footer-links {
