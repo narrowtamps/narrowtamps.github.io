701abdcc505564ababb40752c00b1d651433d551
narrowtamps <narrowtamps@gmail.com>
2026-03-04 01:45:25 -0600

    Update check.js

--- pages/neon/starworks/check.js
+++ pages/neon/starworks/check.js
@@ -18,8 +18,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 console.log("Firebase connected!", app);
-window.function() myFunction {
-    // Your JavaScript code goes here
-    alert("The script from the external file is running!");
-    console.log("Function executed successfully.");
-}
+
