acaca43113920adecca1e44b43aba876fc948f6c
narrowtamps <narrowtamps@gmail.com>
2026-03-04 00:34:19 -0600

    Update check.js

--- pages/neon/starworks/check.js
+++ pages/neon/starworks/check.js
@@ -6,12 +6,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/fireba
 
 // 2. Paste your unique configuration here (copied from the console)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB86jNHj0pT_GmxZ-3mPOXOJiRr-QLEqSA",
+  authDomain: "test-608eb.firebaseapp.com",
+  projectId: "test-608eb",
+  storageBucket: "test-608eb.firebasestorage.app",
+  messagingSenderId: "649362329355",
+  appId: "1:649362329355:web:cbac23cadf9e25e7f35c0e"
 };
 
 // 3. Initialize Firebase by passing the config object
