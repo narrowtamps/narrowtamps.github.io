adbd595e7ab83b9b80280bb00db310337b9e5d7e
narrowtamps <narrowtamps@gmail.com>
2026-03-06 00:27:48 -0600

    Update script-footer-neon.js

--- pages/script-footer-neon.js
+++ pages/script-footer-neon.js
@@ -202,7 +202,7 @@ document.getElementById('footer-placeholder').innerHTML = `
         <a href="https://www.narrowtamps.com/pages/campaigns.html">Campaigns</a>
         <a href="https://www.narrowtamps.com/pages/socials.html">Socials</a>
             <a href="https://www.narrowtamps.com/about.html">About</a>
-            <a href="https://www.narrowtamps.com/fish.html">Fish</a>
+            /*<a href="https://www.narrowtamps.com/fish.html">Fish</a>*/
     </div>
     <p>&copy; 2026 The Narrow Tamps. All rights reserved.</p>
    
